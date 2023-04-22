755343bfb847f56348b561cdd97006404f34de8a
Jgp19112003 <92918174+Jgp19112003@users.noreply.github.com>
2023-04-22 18:24:47 +0200

    .

--- OnlyGames/src/app/services/cargar-scripts.service.ts
+++ OnlyGames/src/app/services/cargar-scripts.service.ts
@@ -9,10 +9,10 @@ export class CargarScriptsService {
 
   Carga(archivos: string[]){
     for (let archivo of archivos) {
-        let script = document.createElement("script")
-        script.src = "./assets/" + archivo + ".js";
-        let body = document.getElementsByTagName("body")[0];
-        body.appendChild(script);
+      let script = document.createElement("script")
+      script.src = "./assets/" + archivo + ".js";
+      let body = document.getElementsByTagName("body")[0];
+      body.appendChild(script);
     }
   }
 }
