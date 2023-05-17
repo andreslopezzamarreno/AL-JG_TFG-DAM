ee2aea669544b10c54f6c6395610ee9c49972693
Jorge_González-dev <jgp19112003@gmail.com>
2023-05-17 13:28:11 +0200

    .

--- OnlyGames/src/app/components/premios/premios.component.ts
+++ OnlyGames/src/app/components/premios/premios.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-premios',
   templateUrl: './premios.component.html',
   styleUrls: ['./premios.component.css']
 })
 export class PremiosComponent {
+  fun(){
+    console.log("click");
+  }
 
 }
+
