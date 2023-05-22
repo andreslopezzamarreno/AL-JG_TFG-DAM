7a3a86c64da6ab193cd51f1aa6c7639257f7fdfe
Andrés López Zamarreño <lopez.zama@gmail.com>
2023-05-22 09:38:25 +0200

    Update app.component.ts

--- OnlyGames/src/app/app.component.ts
+++ OnlyGames/src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {}
 
 /*
-          IMPORTANTE
-          1,9,10
+          IMPORTANCIA
+          1,5,7,8,9,10,4,2,3,6   documentacion   11
 
 
           "JUEGOS"
@@ -21,23 +21,23 @@ export class AppComponent {}
           3. Si es asi lo canjea y le envia email o le sale un alert con el codigo
 
           "EDITAR PERFIL"
-          4. Que el usuario pueda cambiar su gametag --> que pueda cambiar su foto de perfil, donde habria varias fotos que pudiera elegir
-
-          "OTROS"
-          5. Hacer Documentacion
+          4. Que el usuario pueda cambiar su gametag
+              - buscar paquete de iconos
+              - que el usuario pueda comprar iconos
+              - que pueda cambiarse la foto  (string de la ruta de la imagen, como los juegos)
 
           "BUGS"
-          6. Cuando estoy con un juego y me voy a otros juegos --> detener la ejecucion del script si es que hay alguno ejecutandose, ejemplo, en el breakout le das a start e inmediatamente vas al menu, se sigue escuchando los sonidos del juego, como si estuvieses jugando aun
+          5. Cuando estoy con un juego y me voy a otros juegos --> detener la ejecucion del script si es que hay alguno ejecutandose, ejemplo, en el breakout le das a start e inmediatamente vas al menu, se sigue escuchando los sonidos del juego, como si estuvieses jugando aun
 
-          7. Al iniciar sesion sigue apareciendo el mensaje de error como parpadeo y luego inicia sesion
+          6. Al iniciar sesion sigue apareciendo el mensaje de error como parpadeo y luego inicia sesion
 
-          8. El el bublesbhoot solucionar lo del player.draw y lo de ball -> no se ni cuándo ni porque sale
+          7. El el bublesbhoot solucionar lo del player.draw y lo de ball -> no se ni cuándo ni porque sale
 
-          9. En el flappy bird cuando juegas, te sales del componente y vuelves al flappy va el doble de rapido
-          10. En el snake tambien pasa lo de arriba
+          8. En el flappy bird cuando juegas, te sales del componente y vuelves al flappy va el doble de rapido
+          9. En el snake tambien pasa lo de arriba
 
-          11. Al enviar solicitud, ver que no puede enviarselo a si mismo y tampoco aun usuario que ya tiene como amigo
+          10. Al enviar solicitud, ver que no puede enviarselo a si mismo y tampoco a un usuario que ya tiene como amigo
 
           "CONFIRMACION CORREO"
-          12. Descomentar el codigo y dejarlo bien antes de entregarlo
+          11. Descomentar el codigo y dejarlo bien antes de entregarlo
     */
