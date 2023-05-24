d340b0e2cac8741c0e33d27f53ba482879718a21
Jorge_González-dev <jgp19112003@gmail.com>
2023-05-24 11:59:04 +0200

    .

--- OnlyGames/src/app/services/database.service.ts
+++ OnlyGames/src/app/services/database.service.ts
@@ -22,15 +22,24 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class DatabaseService {
-  firebaseConfig = {
-    apiKey: 'AIzaSyBrqX1EVnN70JYSGQdn-2SZRYwxtIqMets',
-    authDomain: 'al-jg-tfg.firebaseapp.com',
-    databaseURL: 'https://al-jg-tfg-default-rtdb.firebaseio.com',
-    projectId: 'al-jg-tfg',
-    storageBucket: 'al-jg-tfg.appspot.com',
-    messagingSenderId: '1004625728225',
-    appId: '1:1004625728225:web:1bdfacd07b2bbd8d861da0',
-    measurementId: 'G-E21591H7RX',
+   firebaseConfig = {
+
+    apiKey: "AIzaSyBrqX1EVnN70JYSGQdn-2SZRYwxtIqMets",
+
+    authDomain: "al-jg-tfg.firebaseapp.com",
+
+    databaseURL: "https://al-jg-tfg-default-rtdb.firebaseio.com",
+
+    projectId: "al-jg-tfg",
+
+    storageBucket: "al-jg-tfg.appspot.com",
+
+    messagingSenderId: "1004625728225",
+
+    appId: "1:1004625728225:web:336928f0ecfdc08c861da0",
+
+    measurementId: "G-WTKRBJ8X6W"
+
   };
   app = initializeApp(this.firebaseConfig);
   db = getFirestore(this.app);
