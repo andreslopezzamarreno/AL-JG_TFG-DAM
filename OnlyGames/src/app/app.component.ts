579cf360fe7ceafe3ea03f2f24e691ff32225871
Andrés López Zamarreño <lopez.zama@gmail.com>
2023-05-28 12:22:28 +0200

    update

--- OnlyGames/src/app/app.component.ts
+++ OnlyGames/src/app/app.component.ts
@@ -13,11 +13,9 @@ export class AppComponent {}
 2. Documentacion
 
 "AMIGOS"
-3. comprobar que si ya son amigos o le ha enviado la solicitud, no deje enviarla
-4. si compras un juego se podria poner una pestaña de confirmacion, o algun mensaje, muy soso el comprar juegos
+3. si compras un juego se podria poner una pestaña de confirmacion, o algun mensaje, muy soso el comprar juegos
 
 "SI SOBRA TIEMPO"
-5. Si es asi lo canjea y le envia email o le sale un alert con el codigo
-6. Cambiar la forma en la que te da el codigo del premio --> alert es un poco feo
+4. Cambiar la forma en la que te da el codigo del premio --> alert es un poco feo
 
 */
--- OnlyGames/src/app/services/email.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Express } from 'express';
-import { CorsRequest } from 'cors';
-import { BodyParser } from 'body-parser';
-@Injectable({
-  providedIn: 'root',
-})
-export class EmailService {}
